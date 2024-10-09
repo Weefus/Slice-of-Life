abb022e58f8faed43be948786f63cf7219aa1ec8
jasperKnutson <jpkaser22@gmail.com>
2024-10-09 14:52:41 -0700

    multiplayer camera
    adjusted camera to follow the first player

--- Assets/jasper k/scripts/followPlayer.cs
+++ Assets/jasper k/scripts/followPlayer.cs
@@ -6,11 +6,13 @@ public class followPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 0, -10);
+    public GameObject[] players;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        players = GameObject.FindGameObjectsWithTag("Player");
+        player = players[0];
     }
 
     // Update is called once per frame
