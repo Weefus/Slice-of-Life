c837157aeb4aa9e8c58c311d4c9fb851c9cdb353
Zeke W <wagnere4606@my.uwstout.edu>
2024-11-06 15:42:04 -0600

    Demo

---
+++ Assets/Zeke_Stuff/Scripts/LLHFinisherState.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LLHFinisherState : MeleeBaseState
+{
+    public override void OnEnter(StateMachine stateMachine)
+    {
+        base.OnEnter(stateMachine);
+
+        // attack
+        attackIndex = 7;
+        duration = 1.5f;
+        animator.SetTrigger("Attack" + attackIndex);
+        // Debug.Log("Player Attack" + attackIndex + "fired!");
+    }
+    public override void OnUpdate(AttackType currentAttack)
+    {
+        base.OnUpdate(currentAttack);
+        if (fixedtime >= duration)
+        {
+            stateMachine.SetNextStateToMain();
+        }
+
+    }
+    public override void OnExit()
+    {
+        base.OnExit();
+        AttackPressedTimer = 0;
+    }
+}
+}
