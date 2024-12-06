169be2ba831757899f127edd62c459a5bfa4e97d
russind2799 <157525294+russind2799@users.noreply.github.com>
2024-12-06 16:17:05 -0600

    One way plat

--- Assets/Devon R_/Scripts/OneWayPlatform.cs
+++ Assets/Devon R_/Scripts/OneWayPlatform.cs
@@ -6,41 +6,40 @@ using UnityEngine.InputSystem;
 public class OneWayPlatform : MonoBehaviour
 {
     //If there is a collision on the platform
-    public bool coll;
+    public GameObject coll;
 
     public PlatformEffector2D platform;
 
     //if yes collision true
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        coll = true;
+        coll = collision.gameObject;
     }
 
     //if no more collision false
     private void OnCollisionExit2D(Collision2D collision)
 
     {
-        coll = false;
+        coll = null;
     }
 
     //Waits to return platform to original state
     IEnumerator Wait()
     {
-        yield return new WaitForSeconds(1f);
+        BoxCollider2D boxCollider = gameObject.GetComponent<BoxCollider2D>();
+        BoxCollider2D playerCollider = coll.GetComponent<BoxCollider2D>();
 
-        //original platform collision arc
-        platform.surfaceArc = 180f;
+        Physics2D.IgnoreCollision(playerCollider, boxCollider);
+        yield return new WaitForSeconds(1f);
+        Physics2D.IgnoreCollision(playerCollider, boxCollider, false);
     }
 
     //Input for when the player presses down
     public void Fall(InputAction.CallbackContext value)
     {
         //Starts the input
-        if (value.started && coll && !CompareTag("Ground"))
-        {
-            //makes the platform intangable
-            platform.surfaceArc = 0f;
-
+        if (value.started && !CompareTag("Ground")) 
+        { 
             //cooldown for the tangability
             StartCoroutine(Wait());
         }
