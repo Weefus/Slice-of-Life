a97e1753fc11f10a2de0c5521876cb729ea7818b
jasperKnutson <jpkaser22@gmail.com>
2024-12-07 14:56:06 -0600

    no delay to the transitional attack
    miku will now begin the second phase immediately after getting to 50% hp or less instead of few of seconds after

--- Assets/blank.cs
+++ Assets/blank.cs
@@ -6,6 +6,15 @@ public class blank : StateMachineBehaviour
 {
 
 
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.GetComponent<basicZombClass>().hp <= (animator.GetComponent<basicZombClass>().maxHP * .5))
+        {
+            animator.SetTrigger("50%<");
+            Debug.Log("trigger");
+        }
+    }
+
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
