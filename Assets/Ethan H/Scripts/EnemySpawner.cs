a8ca2f63d00b4e2958b9886ec03bc290fc3fa37c
LeonardoChase <chase@denfam.com>
2024-12-08 21:03:42 -0600

    zombie with no arms and randomized zombies added

--- Assets/Ethan H/Scripts/EnemySpawner.cs
+++ Assets/Ethan H/Scripts/EnemySpawner.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public basicZombClass enemyPrefab;
+    [SerializeField] public basicZombClass[] enemyPrefabs;
+    public int prefab;
     float vx;
     float vy;
     bool hasSpawned = false;
     // Start is called before the first frame update
     void Start()
     {
+        prefab = Random.Range(0, enemyPrefabs.Length);
         vx = transform.position.x;
         vy = transform.position.y;
     }
@@ -19,14 +21,14 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (hasSpawned == false && collision.CompareTag("CameraRadius"))
         {
-            Instantiate(enemyPrefab, new Vector3(vx, vy, 4.33f), Quaternion.identity);
+            Instantiate(enemyPrefabs[prefab], new Vector3(vx, vy, 4.33f), Quaternion.identity);
             hasSpawned = true;
         }
     }
